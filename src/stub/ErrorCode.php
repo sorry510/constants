f0c43edf2396b792c132b239df327943883afca5
sorry510 <491559675@qq.com>
2021-04-26 11:12:17 +0800

    feat: 添加3个常量

--- src/stub/ErrorCode.php
+++ src/stub/ErrorCode.php
@@ -15,4 +15,19 @@ class ErrorCode
      * @Message("操作失败")
      */
     const ERROR = 400;
+
+    /**
+     * @Message("未登录")
+     */
+    const NO_LOGIN = 401;
+
+    /**
+     * @Message("你的账号已被封禁，请联系系统管理员！")
+     */
+    const USER_FREEZE = 402;
+
+    /**
+     * @Message("没有权限")
+     */
+    const NO_AUTH = 403;
 }
